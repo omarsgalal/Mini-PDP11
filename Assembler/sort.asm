750242e9e91505052ad2d2443d61f1e90e893464
abdokaseb <abdokaseb@gmail.com>
2018-12-19 23:02:55 +0200

    edit sort assembly

--- Assembler/sort.asm
+++ Assembler/sort.asm
@@ -2,13 +2,13 @@ MOV N, R0
 ; initialization
 MOV 1, (R0)+
 MOV 5, (R0)+
-MOV 6, (R0)+
+MOV 2, (R0)+
 MOV 7, (R0)+
 MOV 8, (R0)+
 MOV 3, (R0)+
 MOV 9, (R0)+
 MOV 4, (R0)+
-MOV 2, (R0)+
+MOV 6, (R0)+
 ; size of array
 MOV 9, R1
 ADD N, R1
@@ -17,20 +17,20 @@ outerloop:
 MOV N, R2
 INC N
 MOV N, R3
+MOV R2, R4
+
 innerloop:
-
-CMP @R3, @R2
-BHS skipSwap
-JSR funSwap
-
-skipSwap:
+CMP @R3, @R4
+BHS skipMin
+MOV R3, R4
+skipMin:
 INC R3
 CMP R1, R3
 BEQ finishIneerLoop
 BR innerloop
-
 finishIneerLoop:
-CMP R2, R3
+JSR funSwap
+CMP R1, R2
 BEQ finishOuterLoop
 BR outerloop
 
@@ -42,8 +42,8 @@ HLT
 
 funSwap:
 MOV @R2, R5
-MOV @R3, @R2
-MOV R5, @R3
+MOV @R4, @R2
+MOV R5, @R4
 RTS
 
-define N 2000
+define N 200
